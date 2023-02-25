05b6ae2cc3797e3e4066150ab1b3e9add7a8a58e
trueBabak <babak.jahani94@gmail.com>
2023-02-25 12:05:22 +0330

    Navbar design v.1

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import Avatar from '../Assets/Images/Avatar.jpg'
+import {
+    AiOutlineHome,
+    AiOutlineFundProjectionScreen,
+    AiOutlineUser,
+  } from "react-icons/ai";
+  import { CgFileDocument } from "react-icons/cg";
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className='w-9/12 px-4 bg-red-500 m-auto flex justify-between items-center text-white py-2'>
+            <div className='Avatar w-16 rounded-full overflow-hidden bg-gray-200 ring-2'>
+                <img src={Avatar}/>
+            </div>
+            <div className='Navbar'>
+                <ul className='flex'>
+                    <li className='px-4 text-xl flex items-center'><AiOutlineHome style={{width:'20px' , marginRight:'5px'}}/> Home</li>
+                    <li className='px-4 text-xl flex items-center'><AiOutlineUser style={{width:'20px' , marginRight:'5px'}}/> About</li>
+                    <li className='px-4 text-xl flex items-center'><AiOutlineFundProjectionScreen style={{width:'20px' , marginRight:'5px'}}/> Project</li>
+                    <li className='px-4 text-xl flex items-center'><CgFileDocument style={{width:'20px' , marginRight:'5px'}}/> Resume</li>
+                </ul>
+            </div>
         </div>
     );
 }
