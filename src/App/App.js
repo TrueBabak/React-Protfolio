162c84a913301d90bd771e676a7ac88c15627cb7
trueBabak <babak.jahani94@gmail.com>
2023-02-25 18:32:40 +0330

    import react particle

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import Navbar from '../Pages/Navbar';
+import Particle from '../Pages/Particle';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-black h-screen">
     <Navbar/>
+    <Particle/>
       Start Project
     </div>
   );
