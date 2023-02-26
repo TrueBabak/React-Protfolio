b1358bd948464ef444799786833e16be8d0fd5a1
Babak <jahanibabak80@gmail.com>
2023-02-26 16:48:16 +0330

    homepage2 structure

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,13 @@ import Navbar from '../Pages/Navbar';
 import Particle from '../Pages/Particle';
 import Home from '../Pages/Home/index'
 import './App.css';
+import Home2 from '../Pages/Home/Home2';
 
 function App() {
   return (
     <div className="App bg-black h-screen">
       <Home />
+      <Home2/>
     </div>
   );
 }
---
+++ src/Pages/Home/Home2.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import HomeImg from '../../Assets/Images/home-main.svg'
+
+const Home2 = ()=> {
+  return (
+    <div className='bg-violet-900 mainSection h-screen'>
+        <div className='flex justify-center items-center w-full h-full'>
+                <div className='flex justify-center items-center w-2/3 h-full m-4'>
+                    <div className='w-full m-auto flex-col items-center font-bold'>
+                        <div className='w-3/4 m-auto text-5xl py-2'>Hi There! 
+                            <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
+                        </div>
+                        <div className='w-3/4 m-auto text-5xl py-2'>I'M <span className='text-purple-500'>Babak Jahani</span></div>
+                        <div className='w-3/4 m-auto text-6xl py-16 text-purple-600 text-left'>
+                            type
+                        </div>
+                    </div>
+                </div>
+                <div className='flex justify-center items-center w-1/3 h-full m-4'>
+                    <img className='w-full h-full' src={HomeImg}/>
+                </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home2;
